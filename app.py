d7af0313258f366f4b9f4b702ba24afb60eb07ee
SamuelPaz10 <111510648+SamuelPaz10@users.noreply.github.com>
2023-02-25 10:45:57 -0600

    Configuring environment.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import pymongo
-from classes import DATA, Dataprocess
+from classes import DATA, Dataprocess, DbMongo
+from dotenv import load_dotenv
 
 def main():
 
+    client, db = DbMongo.getDB()
+    
     pipeline = Dataprocess(DATA)
     
     pipeline.create_careers()
     pipeline.create_students()
     pipeline.create_enrollments()
 
+
+    client.close()
     return True
 
+    
+
 if __name__ == "__main__":
+    load_dotenv()
     main()
